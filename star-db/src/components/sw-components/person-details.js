de974ee1229bfadabd9991d747462f54fe1de058
unknown <alexeysvyridov@gmail.com>
2020-09-20 18:15:41 +0300

    s

--- star-db/src/components/sw-components/person-details.js
+++ star-db/src/components/sw-components/person-details.js
@@ -1,19 +1,14 @@
 import React from 'react'
 import ItemDetails, { Record } from '../item-details'
 import { withSwapiService } from '../hoc-helpers'
-const PersonDetails = ({itemId, getImageUrl, getData}) => {
+const PersonDetails = (props) => {
   return (
-    <ItemDetails
-      itemId={itemId}
-      getData={getData}
-      getImageUrl={getImageUrl}
-    >
+    <ItemDetails {...props}    >
       <Record field="gender" label="Gender" />
       <Record field="eyeColor" label="Eye Color" />
     </ItemDetails>
   )
 }
-
 const mapMethodsTopProps = (swapiService) => {
   return {
     getData: swapiService.getPerson,
